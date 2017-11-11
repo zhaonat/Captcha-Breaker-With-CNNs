bb84011211f9b0aa91fa94b2a3a161104f2f4c12
Nathan Zhao <zhaonat@gmail.com>
2017-11-10 21:32:21 -0800

    captcha_editing and creator

--- ConvolutionalNeuralNets/CNN_overlay.py
+++ ConvolutionalNeuralNets/CNN_overlay.py
@@ -5,12 +5,15 @@ from keras.layers import Conv2D, MaxPooling2D
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.utils import np_utils
+dir= 'D:\\Documents\\CS229\\Project\\ConvolutionalNeuralNets\\'
 
-data = pickle.load(open('singlechar_database.p', 'rb'));
+data = pickle.load(open(dir+'overlaychar_database.p', 'rb'));
 
 images = data[0];
 print(images.shape)
 features = data[1];
+print(features.shape)
+features = features[:,0];
 print(images[0].shape)
 print(features)
 #convert all chars to integers
@@ -23,12 +26,12 @@ print(y_train.shape)
 
 ## process images
 ## convert X to flaots
-# images = images.astype('float32');
-# images = images/255.0;
+images = images.astype('float32');
+# images = images/255.0; #FEATURE NORMALIZATION APPEARS TO ADVERSELY AFFECT THIS
 X = images;
 X = X[:,:,:,0:2];
 X = X[:,:,:,0];
-X = np.reshape(X, (5000,60,40,1));
+X = np.reshape(X, (8000,60,40,1));
 ## Create the  convolutional neural net
 
 
@@ -41,32 +44,38 @@ model = Sequential();
 #third argumen
 model.add(Conv2D(20,(2,2),strides = 1, activation = 'relu', input_shape = (60,40,1)))
 model.add(Dropout(0.1));
+
 model.add(Conv2D(40,(5,5),strides = 1, activation = 'relu'))
 model.add(Dropout(0.1));
+model.add(MaxPooling2D(pool_size = (2,2))); #pool size = reduction factor
+
 model.add(Conv2D(80,(6,6),strides = 1, activation = 'relu'))
 model.add(Dropout(0.1));
+
 model.add(Conv2D(160,(2,2),strides = 1, activation = 'relu'))
 model.add(Dropout(0.1));
+model.add(MaxPooling2D(pool_size = (2,2))); #pool size = reduction factor
+
 model.add(Conv2D(80,(2,2),strides = 1, activation = 'relu'))
 model.add(Dropout(0.1));
+
 model.add(Conv2D(40,(3,3),strides = 1, activation = 'relu'))
-# maxpooling essentially does a dimensionality reduction
+
+model.add(Conv2D(20,(1,1),strides = 1, activation = 'relu'))
 model.add(MaxPooling2D(pool_size = (2,2))); #pool size = reduction factor
 
 model.add(Flatten()) #flattens the input (so it's 1d after this point)
-#Dense is just a normal neural network layer (the core layer)
 
 ## only after convolution do we start appending dense layers like a NN
 
 
-
-model.add(Dense(300, activation = 'relu'))
+model.add(Dense(500, activation = 'relu'))
+model.add(Dropout(0.01));
+model.add(Dense(400, activation = 'relu'))
 model.add(Dropout(0.01));
 model.add(Dense(200, activation = 'relu'))
 model.add(Dropout(0.01));
 model.add(Dense(100, activation = 'relu'))
-model.add(Dropout(0.01));
-model.add(Dense(50, activation = 'relu'))
 model.add(Dropout(0.01));
 num_classes = y_train.shape[1]
 model.add(Dense(num_classes, activation = 'softmax'));
@@ -79,5 +88,26 @@ model.compile(loss='categorical_crossentropy',
 
 ## fit the model to the train
 #careful about batch size, can lead to nonetype is not callable error
-model.fit(X, y_train, batch_size=100, epochs=2, verbose=1);
 
+## this appears to be sensitive to number of epochs
+## why is the result here sensitive to the number of epochs...as well as the batch size
+
+history = model.fit(X, y_train, validation_split=0.33, batch_size=200, epochs=100, verbose=1);
+
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+# summarize history for loss
+
+plt.figure()
+plt.plot(history.history['loss'])
+
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
