8d5491829b812b1be4f1037365116463fe92e91b
Nathan Zhao <zhaonat@gmail.com>
2017-11-24 12:49:29 -0800

    more transfer learning

--- TransferLearningWithKeras/TransferFromImageNet.py
+++ TransferLearningWithKeras/TransferFromImageNet.py
@@ -3,8 +3,10 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.models import Sequential, Model
 from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
+from keras.layers import Conv2D
 from keras import backend as k
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
+import pickle
 
 img_width, img_height = 256, 256
 train_data_dir = "data/train"
@@ -18,13 +20,16 @@ model = applications.VGG19(weights = "imagenet", include_top=False, input_shape 
 print(model.summary())
 
 ## step 1: freeze layers that we do not want to tune
-for layer in model.layers[:5]:
+for layer in model.layers[1:5]:
     layer.trainable = False
 
 
 ## add custom layers (which we tune so we can train on our captchas)
 ## one of the layers that we must change is the input layer!
+y = model.input; ## STILL NEEDS WORK
+y.add(Conv2D(20,(2,2),strides = 1, activation = 'relu', input_shape = (60,40,1)))
 
+## add in some extra layers into the output
 x = model.output
 x = Flatten()(x)
 x = Dense(1024, activation="relu")(x)
@@ -33,7 +38,7 @@ x = Dense(1024, activation="relu")(x)
 predictions = Dense(26, activation="softmax")(x)
 
 # creating the final model
-model_final = Model(input = model.input, output = predictions)
+model_final = Model(input = y, output = predictions)
 
 # compile the model
 model_final.compile(loss = "categorical_crossentropy",\
